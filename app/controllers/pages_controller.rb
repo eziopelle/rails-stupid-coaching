b92e5e5cd5bd5481315811533bc85c6535df7133
Victor Pelle <victor.pelle97@gmail.com>
2022-11-14 12:37:20 +0100

    complete

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @ask = params[:answer]
+    if params[:answer] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:answer].include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+    return @answer
+  end
+end
